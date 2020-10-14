1709f6379b0af189133f32eddc9fd014756f6e93
verysetiawan <kangphery@gmail.com>
2020-10-15 01:42:07 +0700

    menambahkan radio name

--- app.py
+++ app.py
@@ -28,10 +28,10 @@ def config():
     # Cetak ip Mikrotik
     print (f"IP Address Mikrotik adalah : {ip_mik}")
     print (f"IP Gateway Router Klien Mikrotik adalah : {ip_gate[:13]}")
-    print (f"host id nya adalah: {ip_mik[12:]}")
+    print (f"host id nya adalah: {ip_mik[11:]}")
 
     #modulus frequency
-    freq = int (ip_mik[12:])
+    freq = int (ip_mik[11:])
     chanel = freq%3
     
     if chanel == 0 :
@@ -56,17 +56,17 @@ def config():
     
     #Perintah konfigurasi router klien     
     config_list = [
-        f"/system identity set name=MUM-AP-{ip_mik[12:]}",
+        f"/system identity set name=MUM-AP-{ip_mik[11:]}",
         '/interface bridge port remove [find interface="wlan1"]',
         "/tool romon set enabled=yes secrets=mum2020",
         "ip service disable telnet,ftp,www,api-ssl",
         '/interface wireless security-profiles set [ find default=yes ] authentication-types=wpa-psk,wpa2-psk eap-methods="" management-protection=allowed mode=dynamic-keys  wpa-pre-shared-key=smkn1nglegok wpa2-pre-shared-key=smkn1nglegok',
-        f'/interface wireless set [ find default-name=wlan1 ] band=2ghz-g/n channel-width=20mhz disabled=no frequency={frekuensi} mode=ap-bridge ssid=MUM-AP-{ip_mik[12:]}',
+        f'/interface wireless set [ find default-name=wlan1 ] band=2ghz-g/n channel-width=20mhz disabled=no frequency={frekuensi} mode=ap-bridge radio-name=MUM-AP-{ip_mik[11:]} ssid=MUM-AP-{ip_mik[11:]}',
         '/interface wireless access-list add interface=wlan1 signal-range=-80..120 vlan-mode=no-tag',
         f"/ip dns set servers={ip_gate[:13]}",
-        f"/ip address add address=172.16.{ip_mik[12:]}.1/24 interface=wlan1 network=172.16.{ip_mik[12:]}.0",
-        f'/ip dhcp-relay add dhcp-server=192.168.100.1 disabled=no interface=wlan1 local-address=172.16.{ip_mik[12:]}.1 name=AP-{ip_mik[12:]}',
-        f"/ip dhcp-server network add address=172.16.{ip_mik[12:]}.0/24 dns-server={ip_gate[:13]} gateway=172.16.{ip_mik[12:]}.1",
+        f"/ip address add address=172.16.{ip_mik[11:]}.1/24 interface=wlan1 network=172.16.{ip_mik[11:]}.0",
+        f'/ip dhcp-relay add dhcp-server=192.168.88.1 disabled=no interface=wlan1 local-address=172.16.{ip_mik[11:]}.1 name=AP-{ip_mik[11:]}',
+        f"/ip dhcp-server network add address=172.16.{ip_mik[11:]}.0/24 dns-server={ip_gate[:13]} gateway=172.16.{ip_mik[11:]}.1",
         "/ip neighbor discovery-settings set discover-interface-list=none",
         "/system ntp client set enabled=yes primary-ntp=202.162.32.12",
         "/system clock set time-zone-name=Asia/Jakarta",
@@ -89,16 +89,16 @@ def config():
     ssh_client.connect(hostname=ip_address_cr,username=username,password=password, allow_agent=False, look_for_keys=False)
     print (f"sukses login to {ip_address_cr}")
     rellay_list = [
-                    f'/ip pool add name=AP-{ip_mik[12:]} ranges=172.16.{ip_mik[12:]}.2-172.16.{ip_mik[12:]}.254',
-                    f'/ip dhcp-server add address-pool=AP-{ip_mik[12:]} disabled=no interface=bridge lease-time=30m name=AP-{ip_mik[12:]} relay=172.16.{ip_mik[12:]}.1',
-                    f'/ip dhcp-server network add address=172.16.{ip_mik[12:]}.0/24 dns-server=8.8.8.8 gateway=172.16.{ip_mik[12:]}.1'
+                    f'/ip pool add name=AP-{ip_mik[11:]} ranges=172.16.{ip_mik[11:]}.2-172.16.{ip_mik[11:]}.254',
+                    f'/ip dhcp-server add address-pool=AP-{ip_mik[11:]} disabled=no interface=bridge lease-time=30m name=AP-{ip_mik[11:]} relay=172.16.{ip_mik[11:]}.1',
+                    f'/ip dhcp-server network add address=172.16.{ip_mik[11:]}.0/24 dns-server=8.8.8.8 gateway=172.16.{ip_mik[11:]}.1'
                     ]
     for rellay in rellay_list:
         ssh_client.exec_command(rellay)
         print (rellay)
         time.sleep(0.2)
 
-    print (f"Berhasil menambahkan rellay pada Core Router untuk network 172.16.{ip_mik[12:]}.0/24 ")
+    print (f"Berhasil menambahkan rellay pada Core Router untuk network 172.16.{ip_mik[11:]}.0/24 ")
     
     return jsonify(data) 
 
